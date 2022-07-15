c85d04ee2e8dd9f6bf82012db2ab04ea2e24d22f
murali0101 <muralimv0101@gmail.com>
2022-07-15 15:09:38 +0530

    done initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { GameScreen } from "./components/GameScreen/GameScreen";
+
+function App() {
+  return (
+    <div className="App">
+      <GameScreen />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GameScreen/GameScreen.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./GameScreen.css";
+export const GameScreen = () => {
+  return (
+    <div className="gameScreen">
+      <div className="gameScreen__heading center">Type The Alphabet</div>
+    </div>
+  );
+};
